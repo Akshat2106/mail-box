48ecec85e1fcc3455827879d2b0ad3b0fd7b3636
Akshat2106 <akshatpatidar2106@gmail.com>
2023-04-08 14:42:45 +0530

    task 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import Navigationbar from './Components/Layout/Navigationbar';
 import Login from './Components/Pages/Login';
 import './App.css'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import DummyScreen from './Components/Layout/DummyScreen';
+import { useSelector } from 'react-redux';
+
 
 function App() {
+  const token = useSelector(state => state.authentication.token)
   return (
     <>
-    <Navigationbar/>
-    <Login/>
+      <Navigationbar />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Login />
+          </Route>
+          <Route exact path='/profile'>
+            {token && <DummyScreen />}
+            {!token && <Redirect to='/' />}
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
--- src/Components/Layout/Navigationbar.js
+++ src/Components/Layout/Navigationbar.js
@@ -3,16 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
 
 const Navigationbar = () => {
     return (
         <Navbar bg="light" variant="light">
             <Container>
-                <Navbar.Brand href="/">MyWebLink</Navbar.Brand>
+                <Navbar.Brand exact to="/">MyWebLink</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/">Products</Nav.Link>
-                    <Nav.Link href="/">About us</Nav.Link>
+                    <Nav.Link exact to="/">Home</Nav.Link>
+                    <Nav.Link exact to="/profile">Welcome</Nav.Link>
+                    <Nav.Link exact to="/">About us</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
